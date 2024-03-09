8bffd66afa2296977c10f7e32c9bcfd13366d02f
ChristoSmith1 <christo.smith@gmail.com>
2024-03-08 19:19:32 -0500

    Gitignore change

--- g_over_t_test.py
+++ g_over_t_test.py
@@ -6,15 +6,15 @@ import g_over_t
 
 g_over_t.hello_world("david")
 powerdata = g_over_t.read_power_file(r"merge_test\MSU_PowerMeter_GoverT_06022024_XXXXUTC_3.csv")
-# print(powerdata)
+print(powerdata)
 
 pointingdata = g_over_t.read_px6_file(r"merge_test\GTprocedure_3.txt")
-# print(pointingdata)
+print(pointingdata)
 
 combineddata= g_over_t.combine_power_position(power_data=powerdata, position_data=pointingdata)
-#print(combineddata)
+print(combineddata)
 
-# g_over_t.write_csv(combineddata, r"test_goverttest.csv")
+g_over_t.write_csv(combineddata, r"test_goverttest.csv")
 
 L = g_over_t.get_column(combineddata, "azimuth")
 print(L)
