de561b98e776f6fffe0f636c958e50bac1bf66b6
ChristoSmith1 <christo.smith@gmail.com>
2024-03-25 13:35:56 -0400

    I attempted to have g over t test read and azimuth unsuccesfully. Cannot figure out the error as yet

--- g_over_t_test.py
+++ g_over_t_test.py
@@ -4,12 +4,12 @@
 
 import g_over_t
 
-g_over_t.hello_world("david")
-powerdata = g_over_t.read_power_file(r"merge_test\MSU_PowerMeter_GoverT_06022024_XXXXUTC_3.csv")
-print(powerdata)
+#g_over_t.hello_world("david")
+powerdata = g_over_t.read_power_file(r"TestingSuite\MSU_PowerMeter_GoverT_09202023_2300UTC_3.csv")
+#print(powerdata)
 
-pointingdata = g_over_t.read_px6_file(r"merge_test\GTprocedure_3.txt")
-print(pointingdata)
+pointingdata = g_over_t.read_px6_file(r"TestingSuite\merge_test\GTprocedure_3.txt")
+#print(pointingdata)
 
 combineddata= g_over_t.combine_power_position(power_data=powerdata, position_data=pointingdata)
 print(combineddata)
@@ -17,4 +17,4 @@ print(combineddata)
 g_over_t.write_csv(combineddata, r"test_goverttest.csv")
 
 L = g_over_t.get_column(combineddata, "azimuth")
-print(L)
+#print(L)
