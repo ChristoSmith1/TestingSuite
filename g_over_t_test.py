39c88ba968325c386c8d4a9bcd8354c0f11d0a05
ChristoSmith1 <christo.smith@gmail.com>
2024-03-25 23:22:19 -0400

    plots

--- g_over_t_test.py
+++ g_over_t_test.py
@@ -21,6 +21,8 @@ g_over_t.write_csv(combineddata, r"test_goverttest.csv")
 power_data_list = g_over_t.get_column(combineddata, "power")
 #print(L)
 
+elevation_data_list = g_over_t.get_column(combineddata, "elevation")
+
 azimuth_data_list = g_over_t.get_column(combineddata, "azimuth")
 #print(M)
 
@@ -31,7 +33,31 @@ print(f"length = {len(timestamp_data_list)}")
 print(f"{len(timestamp_data_list)=}")
 print(f"{len(azimuth_data_list)=}")
 
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2,layout = "constrained")
+fig.suptitle('Power vs. Time and Azimuth vs. Elevation')
 
+ax1.plot(timestamp_data_list,power_data_list)
+ax1.set_title('power vs. time')
+ax1.set_ylabel('power in dB')
+ax1.set_xlabel('Day + Time in UTC')
+
+ax2.plot(timestamp_data_list,azimuth_data_list, 'tab:green')
+ax2.set_title('time vs. az')
+ax2.set_xlabel("time")
+ax2.set_ylabel("azimuth")
+
+ax3.plot(timestamp_data_list,elevation_data_list)
+ax3.set_title('time vs. el')
+ax3.set_xlabel("time")
+ax3.set_ylabel("elevation")
+
+ax4.plot(elevation_data_list,power_data_list, 'tab:red')
+ax4.set_title('el vs. power')
+ax4.set_xlabel("power")
+ax4.set_ylabel("elevation")
+
+# fig.tight_layout()
+plt.show()
 #need to adjust my code for actual analysis for elevation column.
     #delta in on moon off moon for "first power delta"
     #use that delta for anaylsis of elevation column
