d4635e9f9785e3e754f5ed238e6f3f2daef73d9a
ChristoSmith1 <christo.smith@gmail.com>
2024-03-26 02:34:35 -0400

    new dataset from tonights test, won't plot

--- g_over_t_test.py
+++ g_over_t_test.py
@@ -7,16 +7,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #g_over_t.hello_world("david")
-powerdata = g_over_t.read_power_file(r"merge_test\MSU_PowerMeter_GoverT_06022024_XXXXUTC_3.csv")
+powerdata = g_over_t.read_power_file(r"C:\Users\chris\thesis\TestingSuite\MSU_PowerMeter_GoverT_03262024_0230UTC_1.csv")
 #print(powerdata)
 
-pointingdata = g_over_t.read_px6_file(r"merge_test\GTprocedure_3.txt")
+pointingdata = g_over_t.read_px6_file(r"C:\Users\chris\thesis\TestingSuite\GTpoint03252024.txt")
 #print(pointingdata)
 
 combineddata= g_over_t.combine_power_position(power_data=powerdata, position_data=pointingdata)
 #print(combineddata)
 
-g_over_t.write_csv(combineddata, r"test_goverttest.csv")
+g_over_t.write_csv(combineddata, r"test_goverttest2.csv")
 
 power_data_list = g_over_t.get_column(combineddata, "power")
 #print(L)
@@ -33,28 +33,30 @@ print(f"length = {len(timestamp_data_list)}")
 print(f"{len(timestamp_data_list)=}")
 print(f"{len(azimuth_data_list)=}")
 
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2,layout = "constrained")
-fig.suptitle('Power vs. Time and Azimuth vs. Elevation')
+plt.plot(timestamp_data_list,power_data_list)
 
-ax1.plot(timestamp_data_list,power_data_list)
-ax1.set_title('power vs. time')
-ax1.set_ylabel('power in dB')
-ax1.set_xlabel('Day + Time in UTC')
+# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2,layout = "constrained")
+# fig.suptitle('Power vs. Time and Azimuth vs. Elevation')
 
-ax2.plot(timestamp_data_list,azimuth_data_list, 'tab:green')
-ax2.set_title('time vs. az')
-ax2.set_xlabel("time")
-ax2.set_ylabel("azimuth")
+# ax1.plot(timestamp_data_list,power_data_list)
+# ax1.set_title('power vs. time')
+# ax1.set_ylabel('power in dB')
+# # ax1.set_xlabel('Day + Time in UTC')
 
-ax3.plot(timestamp_data_list,elevation_data_list)
-ax3.set_title('time vs. el')
-ax3.set_xlabel("time")
-ax3.set_ylabel("elevation")
+# ax2.plot(timestamp_data_list,azimuth_data_list, 'tab:green')
+# ax2.set_title('time vs. az')
+# # ax2.set_xlabel("time")
+# ax2.set_ylabel("azimuth")
 
-ax4.plot(elevation_data_list,power_data_list, 'tab:red')
-ax4.set_title('el vs. power')
-ax4.set_xlabel("power")
-ax4.set_ylabel("elevation")
+# ax3.plot(timestamp_data_list,elevation_data_list)
+# ax3.set_title('time vs. el')
+# # ax3.set_xlabel("time")
+# ax3.set_ylabel("elevation")
+
+# ax4.plot(elevation_data_list,power_data_list, 'tab:red')
+# ax4.set_title('el vs. power')
+# ax4.set_xlabel("power")
+# ax4.set_ylabel("elevation")
 
 # fig.tight_layout()
 plt.show()
