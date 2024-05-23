6c885df01d4e3415f281b0f2eb767062c9cb977d
ChristoSmith1 <christo.smith@gmail.com>
2024-05-23 18:34:19 -0400

    cluster analysis

--- combined_filtered_analysis.py
+++ combined_filtered_analysis.py
@@ -30,33 +30,49 @@ filtered_data["elapsed"] = filtered_data["timestamp_posix"] - timemin
 print(filtered_data)
 print(filtered_data.dtypes)
 
-fig,ax = plt.subplots()
-ax: plt.Axes
-ax.plot(filtered_data["timestamp_posix"],filtered_data["power"])
-plt.show()
+# fig,ax = plt.subplots()
+# ax: plt.Axes
+# ax.plot(filtered_data["timestamp_posix"],filtered_data["power"])
+# plt.show()
+
+def find_clusters(
+    on_moon_power: pd.DataFrame
+        
+):
+    elapsed_times = list(on_moon_power["elapsed"])
+    cluster = []
+    while elapsed_times:
+        first_item = elapsed_times.pop(0)
+        if not cluster:
+            cluster.append(first_item)
+        # if #elapsed times are in close proximity
+        #     #pass it to a "cluster"
+        # else # look at next time
+        #     #repeat process
+        pass
 
 def y_factor_criteria(
     data: pd.DataFrame,
     threshold_value: float = 0.05
 ):
     max_power = max(data["power"])
-    min_power = min(data["power"])
+
     power_threshold_min = max_power - threshold_value
-    power_threshold_max = min_power + threshold_value
-    y_factor_max = data.loc[data["power"]>=power_threshold_min]
-    y_factor_min = data.loc[data["power"]<=power_threshold_max]
+    
+    on_moon_power: pd.DataFrame = data.loc[data["power"]>=power_threshold_min]
+
+
+    
     fig,[ax0, ax1] = plt.subplots(2)
     ax0: plt.Axes
     ax1: plt.Axes
-    ax0.scatter(y_factor_max["elapsed"],y_factor_max["power"])
-    ax0.plot(y_factor_max["elapsed"],y_factor_max["power"])
-    ax1.scatter(y_factor_min["elapsed"],y_factor_min["power"])
-    ax1.plot(y_factor_min["elapsed"],y_factor_min["power"])
+    ax0.scatter(on_moon_power["elapsed"],on_moon_power["power"])
+    ax0.plot(on_moon_power["elapsed"],on_moon_power["power"])
 
     min_elapsed = min(data["elapsed"])
     max_elapsed = max(data["elapsed"])
     ax0.set_xlim(min_elapsed,max_elapsed)
-    ax1.set_xlim(min_elapsed,max_elapsed)
+
 
     plt.show()
 
