eaf51e766dbebc377773be958d7b540612107249
ChristoSmith <christo.smith@gmail.com>
2024-09-04 20:14:37 -0400

    Add function to save fig and json

--- ChristoGoverT_beta.py
+++ ChristoGoverT_beta.py
@@ -31,90 +31,91 @@ def choose_device():
     chosen_device = test.open_resource(chosen_device_name)
     return chosen_device
     
-    # while True:
-    #     try:
-    #         device = list(test.list_resources())[int(input("Device Num: "))]
-    #         break
-    #     except ValueError:
-    #         print("Please input an integer")
+if __name__ == "__main__":
+        # while True:
+        #     try:
+        #         device = list(test.list_resources())[int(input("Device Num: "))]
+        #         break
+        #     except ValueError:
+        #         print("Please input an integer")
 
-    # power_meter = test.open_resource(device)
-#######################################
-power_meter=choose_device()
+        # power_meter = test.open_resource(device)
+    #######################################
+    power_meter=choose_device()
 
-start_time = time.time() #start time of operations of the actual machine, and reading of data
+    start_time = time.time() #start time of operations of the actual machine, and reading of data
 
-# #################
-# rm = pyvisa.ResourceManager() #This lookes at all of the GPIB available resources and gives their addresses in a particular format
-# print(rm.list_resources()) #I want to make it so it lets me CHOOSE which resource I want to use, so that power meter is based on my input "choose insturment [1]etc."
+    # #################
+    # rm = pyvisa.ResourceManager() #This lookes at all of the GPIB available resources and gives their addresses in a particular format
+    # print(rm.list_resources()) #I want to make it so it lets me CHOOSE which resource I want to use, so that power meter is based on my input "choose insturment [1]etc."
 
-# power_meter = rm.open_resource('GPIB2::13::INSTR') #connects to the insturment of our choosing, hardcoded in at the moment.
-# #################
+    # power_meter = rm.open_resource('GPIB2::13::INSTR') #connects to the insturment of our choosing, hardcoded in at the moment.
+    # #################
 
-print(power_meter.query('*IDN?')) #this asks the insturment to print its GPIB address, serial number and firmware version and works.
+    print(power_meter.query('*IDN?')) #this asks the insturment to print its GPIB address, serial number and firmware version and works.
 
-print(power_meter.query('SYST:RINT?')) #this is to make sure you are using the correct address interface
+    print(power_meter.query('SYST:RINT?')) #this is to make sure you are using the correct address interface
 
-########TESTING AND CALIBRATION THAT CAN BE AUTOMATED ########
-# cal_factor = input("what is the CF percentage for your IF input?")
-# message = 'RCF 98.7PCT'
-# print(f"About to write {message}")
-# response = power_meter.write(message)
-# print(f"Received response: {response}")
-# exit()
-print(power_meter.write('CAL')) #this calibrates the insturment
-sleep(18)
-print(power_meter.write('ZE')) #this zeroe's the insturment
-sleep(10)
-power_meter.write('*TST?') # This makes the machine conduct a self test before beginning
-sleep(10)
-#the sleep cycles in the machine cal process above are their because while they are running, if I attempt to read out power it will crash
+    ########TESTING AND CALIBRATION THAT CAN BE AUTOMATED ########
+    # cal_factor = input("what is the CF percentage for your IF input?")
+    # message = 'RCF 98.7PCT'
+    # print(f"About to write {message}")
+    # response = power_meter.write(message)
+    # print(f"Received response: {response}")
+    # exit()
+    print(power_meter.write('CAL')) #this calibrates the insturment
+    sleep(18)
+    print(power_meter.write('ZE')) #this zeroe's the insturment
+    sleep(10)
+    power_meter.write('*TST?') # This makes the machine conduct a self test before beginning
+    sleep(10)
+    #the sleep cycles in the machine cal process above are their because while they are running, if I attempt to read out power it will crash
 
-print(power_meter.query('*OPC?')) #this is the SCIP protocol for telling me operations are completed, currently prints "+5" implying syntax error
-sleep(1)
-input("Press Enter to continue...")
+    print(power_meter.query('*OPC?')) #this is the SCIP protocol for telling me operations are completed, currently prints "+5" implying syntax error
+    sleep(1)
+    input("Press Enter to continue...")
 
-######## USER INPUTS #########
-number_of_readings =  int(input("How many times do you want to read?")) #This is the number of readings we wish to take
-pause_between_readings = float(input("How long do you want to pause between readings? Between .1 and 10 seconds"))
-#if pause < 0:
-#    print ("Please enter a positive number between 0.0 and 10")
-#    pause_between_readings = float(input("How long do you want to pause between readings?"))
-#print (pause_between_readings) #this is how long we pause between them
+    ######## USER INPUTS #########
+    number_of_readings =  int(input("How many times do you want to read?")) #This is the number of readings we wish to take
+    pause_between_readings = float(input("How long do you want to pause between readings? Between .1 and 10 seconds"))
+    #if pause < 0:
+    #    print ("Please enter a positive number between 0.0 and 10")
+    #    pause_between_readings = float(input("How long do you want to pause between readings?"))
+    #print (pause_between_readings) #this is how long we pause between them
 
-# number_of_readings = 100 #This is the hard coded value for the first test we did on May 2, 2023
-# pause_between_readings = 0.10 #This is the hard coded value for the first test we did on May 2, 2023
-
-
-# #iter and pause are simple and useable but should be able to handle things like "hey you can't put a negative number here, or 'thats a letter!'""
-
-############# CREATION OF CSV AND WRITING OF POWERS ###############
-#Take user input for file naming convention
-filenumber = input("Input file number (1,2,etc.): ")
-
-#opens a titles a CSV, should be in format Power_Reading_DISHNAME_Date.csv
-file_name = "data/MSU_PowerMeter_GoverT_" + str(datetime.utcnow().isoformat(timespec="minutes")) + filenumber + ".csv"
-file_name = file_name.replace(":","_").replace("-","_").replace(" ","_")
-print(f"using file name: {file_name}")
-with open(file_name, "w") as file:
-# with open("MSU_PowerMeter_GoverT_01022024_YYYYUTC_" +"XXXX"+ ".txt", "w") as file:
-     #these arrays are only generated below for the plotting of the data after completion of the testing
-     array_p = []
-     array_t = []
-     for i in range(number_of_readings):
-         power = round(float(power_meter.query('FETC:POW:AC?')),2)
-         array_p.append(power)
-         file.write(f"{datetime.utcnow()},{str(power)}\n")
-         array_t.append(datetime.utcnow())
-         print(i, datetime.utcnow(), power, "dBm") #printed for user benefit, to see how many iterations are left
-         sleep(pause_between_readings)
-
-print(f"Average = {sum(array_p) / len(array_p):0.2f}")
-
-end_time = time.time() #this is the time the actual data gathering portion ends
-elapsed_time = end_time - start_time #elapsed time helps us dial in how many readings we think will be useful and the time delays that match pointing the dish
-print('Execution time of', number_of_readings, 'readings took' , elapsed_time, 'seconds') #execution time helps dial in the iterations and sleep times based on how long we have.
+    # number_of_readings = 100 #This is the hard coded value for the first test we did on May 2, 2023
+    # pause_between_readings = 0.10 #This is the hard coded value for the first test we did on May 2, 2023
 
 
-#RESOURCES
-#https://naic.edu/~phil/hardware/Misc/powerMeters/E4418-90029_prog_man.pdf
+    # #iter and pause are simple and useable but should be able to handle things like "hey you can't put a negative number here, or 'thats a letter!'""
+
+    ############# CREATION OF CSV AND WRITING OF POWERS ###############
+    #Take user input for file naming convention
+    filenumber = input("Input file number (1,2,etc.): ")
+
+    #opens a titles a CSV, should be in format Power_Reading_DISHNAME_Date.csv
+    file_name = "data/MSU_PowerMeter_GoverT_" + str(datetime.utcnow().isoformat(timespec="minutes")) + filenumber + ".csv"
+    file_name = file_name.replace(":","_").replace("-","_").replace(" ","_")
+    print(f"using file name: {file_name}")
+    with open(file_name, "w") as file:
+    # with open("MSU_PowerMeter_GoverT_01022024_YYYYUTC_" +"XXXX"+ ".txt", "w") as file:
+        #these arrays are only generated below for the plotting of the data after completion of the testing
+        array_p = []
+        array_t = []
+        for i in range(number_of_readings):
+            power = round(float(power_meter.query('FETC:POW:AC?')),2)
+            array_p.append(power)
+            file.write(f"{datetime.utcnow()},{str(power)}\n")
+            array_t.append(datetime.utcnow())
+            print(i, datetime.utcnow(), power, "dBm") #printed for user benefit, to see how many iterations are left
+            sleep(pause_between_readings)
+
+    print(f"Average = {sum(array_p) / len(array_p):0.2f}")
+
+    end_time = time.time() #this is the time the actual data gathering portion ends
+    elapsed_time = end_time - start_time #elapsed time helps us dial in how many readings we think will be useful and the time delays that match pointing the dish
+    print('Execution time of', number_of_readings, 'readings took' , elapsed_time, 'seconds') #execution time helps dial in the iterations and sleep times based on how long we have.
+
+
+    #RESOURCES
+    #https://naic.edu/~phil/hardware/Misc/powerMeters/E4418-90029_prog_man.pdf
--- LISA_T_TEST.py
+++ LISA_T_TEST.py
@@ -1,32 +1,67 @@
+import json
 import pyvisa
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 from time import sleep
 
+
 test = pyvisa.ResourceManager()
 
-for i, v in enumerate(list(test.list_resources())):
-    print(f"[{i+1}] {v}")
-while True:
-    try:
-        # device = list(test.list_resources())[int(input("Device Num: "))-1]
-        device = list(test.list_resources())[4]
-        break
-    except ValueError:
-        print("Please input an integer")
+def choose_device():
+    resources = list(test.list_resources())
+    for index, resource in enumerate(resources):
+        resource_id = "todo"
+        try:
+            listed_device = test.open_resource(resource)
+            query_response: str = listed_device.query("CF?")
+            resource_id = query_response.strip()
+            # print (f"{query_response = } ")
+        except Exception as exc:
+            resource_id = "not available"
+        print(f"[{index}] {resource} {resource_id}")
 
-DUT = test.open_resource(device)
+    user_input = input("select a number for your device: ")
+    user_input_int = int(user_input)
+    chosen_device_name = resources[user_input_int]
+    chosen_device = test.open_resource(chosen_device_name)
+    return chosen_device
 
-# CF = DUT.write("CF=22680000")
-units = str(DUT.query("AUNITS?")).strip()
-stFreq = float(DUT.query("FA?"))
-enFreq = float(DUT.query("FB?"))
-traceDataRaw = DUT.query("TRA?").split(",")
+def write_to_resource(resource: pyvisa.resources.Resource, message: str) -> int:
+    print (f"about to write: {message}")
+    response = resource.write(message)
+    print(f"received response: {response}   type= {type(response)}")
+    return response
+
+def query_resource(resource: pyvisa.resources.Resource, message: str) -> str:
+    print (f"about to query: {message}")
+    response = resource.query(message)
+    print(f"received response: {response}   type= {type(response)}")
+    return response
+
+DUT = choose_device()
+# for i, v in enumerate(list(test.list_resources())):
+#     print(f"[{i+1}] {v}")
+# while True:
+#     try:
+#         device = list(test.list_resources())[int(input("Device Num: "))-1]
+#         # device = list(test.list_resources())[4]
+#         break
+#     except ValueError:
+#         print("Please input an integer")
+
+# DUT = test.open_resource(device)
+
+CF = write_to_resource(DUT,"CF=22680000")
+units = str(query_resource(DUT,"AUNITS?")).strip()
+stFreq = float(query_resource(DUT,"FA?"))
+enFreq = float(query_resource(DUT,"FB?"))
+traceDataRaw = query_resource(DUT,"TRA?").split(",")
 traceData = np.array([float(i) for i in traceDataRaw])
 # frequencies = [stFreq+(((enFreq-stFreq)/len(traceData))*i) for i in range(len(traceData))]
 frequencies = [round(stFreq+(((enFreq-stFreq)/600)*i)) for i in range(601)]
 
+# exit()
 peak = float(DUT.write("MKPK"))
 center = float(DUT.query("CF?"))
 markerFreq = float(DUT.query("MKF?"))
@@ -35,25 +70,63 @@ markerPow = float(DUT.query("MKA?"))
 print(f"CF:     {markerFreq}")
 print(f"CF POW: {markerPow}")
 
-timed = 1000
+timed = 10
 
-# plt.plot(frequencies, traceData)
-# plt.xlabel("Frequency [Hz]")
-# plt.ylabel(f"Amplitude [{units}]")
-# plt.title(f"Plot {stFreq} Hz - {enFreq} Hz")
-# plt.ylim(top=10)
-# plt.yticks()
+plt.plot(frequencies, traceData)
+plt.xlabel("Frequency [Hz]")
+plt.ylabel(f"Amplitude [{units}]")
+plt.title(f"Plot {stFreq} Hz - {enFreq} Hz")
+plt.ylim(top=10)
+plt.yticks()
+
+def save_plot_and_csv(index: int) -> None:
+    print(f"SAVING PLOT!!!")
+    units = str(query_resource(DUT,"AUNITS?")).strip()
+    stFreq = float(query_resource(DUT,"FA?"))
+    enFreq = float(query_resource(DUT,"FB?"))
+    traceDataRaw = query_resource(DUT,"TRA?").split(",")
+    traceData = np.array([float(i) for i in traceDataRaw])
+    # frequencies = [stFreq+(((enFreq-stFreq)/len(traceData))*i) for i in range(len(traceData))]
+    frequencies = [round(stFreq+(((enFreq-stFreq)/600)*i)) for i in range(601)]
+    plt.plot(frequencies, traceData, color="blue")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel(f"Amplitude [{units}]")
+    plt.title(f"Plot {stFreq} Hz - {enFreq} Hz\n{datetime.utcnow()}")
+    plt.ylim(top=10)
+    plt.yticks()
+    filename = datetime.utcnow().isoformat(timespec="seconds").replace(":", "_")
+    filepath = f"data/lisa_t/{filename}.png"
+    print(f"SAVING IMAGE TO {filepath}")
+
+    filepath_json = filepath.replace(".png", ".json")
+
+    dictionary = {}
+
+    timestamp = datetime.utcnow()
+    dictionary["timestamp"] = timestamp.isoformat()
+    for freq, amp in zip(frequencies, traceData):
+        dictionary[freq] = amp
+    print(f"SAVING JSON to: {filepath_json}")
+    with open(filepath_json, "w") as file:
+        json.dump(dictionary, file, indent=2)
+    plt.savefig(filepath)
+    plt.clf()
 
 timeStamp = datetime.now().timestamp()
 with open(f"LISA_T_TEST2.csv", "w") as file:
     file.write("Datetime [UTC],  Center Freq [Hz], Marker Freq [Hz], Marker Power [dBm]\n")
-    for i in range (timed):
-        # peak = float(DUT.write("MKPK"))
-        # center = float(DUT.query("CF?"))
-        # markerFreq = float(DUT.query("MKF?"))
-        # markerPow = float(DUT.query("MKA?"))
-        file.write(f"{datetime.utcnow()},{center},{peak},{markerFreq},{markerPow}\n")
+    for index in range (timed):
+        peak = float(DUT.write("MKPK"))
+        center = float(DUT.query("CF?"))
+        markerFreq = float(DUT.query("MKF?"))
+        markerPow = float(DUT.query("MKA?"))
+        message = f"{datetime.utcnow()},{center},{peak},{markerFreq},{markerPow}"
+        print(f"{index=}: " + message)
+        file.write(message + "\n")
         sleep(1.5)
+
+        if index % 4 == 0:
+            save_plot_and_csv(index=index)
 # plt.savefig(f"AstroForgePlots/Plot_{titleMod}_{stFreq}-{enFreq}_{timeStamp}.png")
 
-plt.show()
+# plt.show()
