7fc8f9a6970af9df8d8a2b8845288cc7a7987917
ChristoSmith1 <christo.smith@gmail.com>
2024-09-16 16:21:11 -0400

    massaging math

--- tip_curve.py
+++ tip_curve.py
@@ -10,12 +10,12 @@ from g_over_t_test import Yfactor
 
 info = sept_info
 
-yfactor = 5.0
+yfactor = 5.2
 #import YFactor and use info to generate average Y-factor from multiple on/off measurements
 
 def tip_curve(info: TestInfo):
 
-    t_op = Top=abs((180-(10**(yfactor/10))*10)/(10**(Yfactor/10)-1))
+    t_op =abs(180-((10**(yfactor/10))*10))/((10**(yfactor/10))-1)
     # Top=abs((135-(10**(Yfactor/10))*10)/(10**(Yfactor/10)-1)) <- 135 moon contribution at S-Band, 180 at X-band
 
     # I need to get just the elevation column from sept_info which right now gets everything.
@@ -23,5 +23,7 @@ def tip_curve(info: TestInfo):
     # T_el = T_op*10**((measured_power_at_any_elevation - off_moon_measurement)/10) <- theoretical math
         #This is why my math should have an "off moon measurement" at cold-sky with an additional attenuation of 25dB (approx 55.1 dB)
 
+    print (t_op)
 
-    pass
+
+tip_curve(sept_info)
